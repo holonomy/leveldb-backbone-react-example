6d99198d0bb4a886aeb866da29ba593595d90d13
Michael Williams <dinosaur@riseup.net>
2014-02-28 00:47:07 -0800

    change from express to just connect

--- src/server.js
+++ src/server.js
@@ -1,22 +1,18 @@
 "use strict";
 
-var express = require('express');
-var fs = require('fs');
-var ecstatic = require('ecstatic');
-var multilevel = require('multilevel');
-var shoe = require('shoe');
-var url = require('url');
-var React = require('react');
-require('node-jsx').install()
-
 var isProd = (process.env.NODE_ENV === "production");
 var isDev = (process.env.NODE_ENV === "development");
 
-var app = express();
-var Page = require('./page-view')
+//
+// http server!
+//
+var connect = require('connect');
+var http = require('http');
 
-app.use(express.compress());
-app.use(ecstatic({
+var app = connect();
+
+app.use(require('compression')());
+app.use(require('ecstatic')({
   root: __dirname + "/../static",
   cache: (isProd ? 3600 : 0),
 }));
@@ -25,18 +21,30 @@ if (isDev) {
   app.use(require('connect-livereload')());
 }
 
+var url = require('url');
+var React = require('react');
+require('node-jsx').install()
+
+var Page = require('./page-view')
 app.use(function (req, res, next) {
   try {
     var path = url.parse(req.url).pathname
     var page = Page({path: path})
     var markup = React.renderComponentToString(page)
-    res.send(markup)
+    res.end(markup)
   } catch(err) {
     return next(err)
   }
 });
 
-var server = app.listen(isProd ? 80 : 5000);
+var server = http.createServer(app);
+server.listen(isProd ? 80 : 5000);
+
+//
+// socket server!
+//
+var multilevel = require('multilevel');
+var shoe = require('shoe');
 
 // db
 var db = require('./serverdb');
